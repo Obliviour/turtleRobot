cbfb96319b3f8e39628c540390f70148ac848791
Obliviour <victor.barr@yahoo.com>
2018-10-15 16:11:13 -0400

    hw7 -p1

--- turtlebot-master/kobuki_bumpers.py
+++ turtlebot-master/kobuki_bumpers.py
@@ -43,37 +43,37 @@ import roslib
 import rospy
 from kobuki_msgs.msg import BumperEvent
 
-class kobuki_button():
+class kobuki_bumper():
 
-	def __init__(self):
-		rospy.init_node("kobuki_bumper")		
+        def __init__(self):
+                rospy.init_node("kobuki_bumper")                
 
-		#monitor kobuki's button events
+                #monitor kobuki's button events
         rospy.Subscriber('mobile_base/events/bumper',BumperEvent,self.BumperEventCallback)
 
-		#rospy.Subscriber("/mobile_base/events/button",ButtonEvent,self.ButtonEventCallback)
+                #rospy.Subscriber("/mobile_base/events/button",ButtonEvent,self.ButtonEventCallback)
 
-		#rospy.spin() tells the program to not exit until you press ctrl + c.  If this wasn't there... it'd subscribe and then immediatly exit (therefore stop "listening" to the thread).
-		rospy.spin();
+        #rospy.spin() tells the program to not exit until you press ctrl + c.  If this wasn't there... it'd subscribe and then immediatly exit (therefore stop "listening" to the thread).
+        rospy.spin();
 
 
-	
-	def BumperEventCallback(self,data):
-	    if ( data.state == BumperEvent.RELEASED ) :
-		state = "released"
-	    else:
-		state = "pressed"  
-	    if ( data.bumper == BumperEvent.LEFT ) :
-		bumper = "Left"
-	    elif ( data.bumper == BumperEvent.RIGHT ) :
-		bumper = "Right"
-	    else:
-		bumper = "Center"
-	    rospy.loginfo("Bumper %s was %s."%(bumper, state))
-	
+        
+        def BumperEventCallback(self,data):
+            if ( data.state == BumperEvent.RELEASED ) :
+                state = "released"
+            else:
+                state = "pressed"  
+            if ( data.bumper == BumperEvent.LEFT ) :
+                bumper = "Left"
+            elif ( data.bumper == BumperEvent.RIGHT ) :
+                bumper = "Right"
+            else:
+                bumper = "Center"
+            rospy.loginfo("Bumper %s was %s."%(bumper, state))
+        
 
 if __name__ == '__main__':
-	try:
-		kobuki_bumper()
-	except rospy.ROSInterruptException:
-		rospy.loginfo("exception")
+        try:
+                kobuki_bumper()
+        except rospy.ROSInterruptException:
+                rospy.loginfo("exception")
