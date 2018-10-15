df85e52452db526dcfa4317dd54585f55b74fdd1
Victor Barr <victor.barr@yahoo.com>
2018-10-15 15:20:00 -0400

    [TUR-001] Initialization of Github
    [Overview] Added Turtulebot-master code, v&h notes from past homeworks, and the goforward.py modified from past use
    
    [Future Progress] Use github to manage in-class projects and files to avoid losing files on local robot directories

---
+++ goforward.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+'''
+Copyright (c) 2015, Mark Silliman
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
+# A very basic TurtleBot script that moves TurtleBot forward indefinitely. Press CTRL + C to stop.  To run:
+# On TurtleBot:
+# roslaunch turtlebot_bringup minimal.launch
+# On work station:
+# python goforward.py
+
+import rospy
+from kobuki_msgs.msg import BumperEvent
+from geometry_msgs.msg import Twist
+
+
+class GoForward():
+    def __init__(self):
+        # initiliaze
+	print('Start')
+        rospy.init_node('GoForward', anonymous=False)
+
+	self.bump_hit = 0
+
+	# tell user how to stop TurtleBot
+	rospy.loginfo("To stop TurtleBot CTRL + C")
+
+        # What function to call when you ctrl + c    
+        rospy.on_shutdown(self.shutdown)
+        
+	# Create a publisher which can "talk" to TurtleBot and tell it to move
+        # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
+        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+     
+	print("Viccy B")	
+	#rospy.init_node('temp_state', anonymous=False)
+        rospy.Subscriber('mobile_base/events/bumper',BumperEvent,self.BumperEventCallback)
+
+	print('Got here')
+	#TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
+
+        r = rospy.Rate(10);
+
+        # Twist is a datatype for velocity
+        move_cmd = Twist()
+	# let's go forward at 0.2 m/s
+        move_cmd.linear.x = 0.2
+	# let's turn at 0 radians/s
+	move_cmd.angular.z = 0
+
+	# as long as you haven't ctrl + c keeping doing...
+        while not rospy.is_shutdown():
+	    print('a;lnkkl;n')
+	    if (self.bump_hit==1):
+
+		rospy.loginfo("sehejjy is life")
+		self.shutdown()
+	    # publish the velocity
+            self.cmd_vel.publish(move_cmd)
+	    # wait for 0.1 seconds (10 HZ) and publish again
+            r.sleep()
+                        
+        
+    def shutdown(self):
+        # stop turtlebot
+        rospy.loginfo("Stop TurtleBot")
+	# a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        self.cmd_vel.publish(Twist())
+	# sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        rospy.sleep(1)
+ 
+    def BumperEventCallback(self,data):
+	if(data.state == BumperEvent.PRESSED):
+		self.bump_hit = 1
+
+if __name__ == '__main__':
+	GoForward()
