607d747c4c0e19b0652ad37c9c0af98b80dc2958
Obliviour <victor.barr@yahoo.com>
2018-10-15 16:16:06 -0400

    retabs

--- goforward_withBump.py
+++ goforward_withBump.py
@@ -29,61 +29,61 @@ from geometry_msgs.msg import Twist
 class GoForward():
     def __init__(self):
         # initiliaze
-	print('Start')
+        print('Start')
         rospy.init_node('GoForward', anonymous=False)
 
-	self.bump_hit = 0
+        self.bump_hit = 0
 
-	# tell user how to stop TurtleBot
-	rospy.loginfo("To stop TurtleBot CTRL + C")
+        # tell user how to stop TurtleBot
+        rospy.loginfo("To stop TurtleBot CTRL + C")
 
         # What function to call when you ctrl + c    
         rospy.on_shutdown(self.shutdown)
         
-	# Create a publisher which can "talk" to TurtleBot and tell it to move
+        # Create a publisher which can "talk" to TurtleBot and tell it to move
         # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
         self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
      
-	print("Viccy B")	
-	#rospy.init_node('temp_state', anonymous=False)
+        print("Viccy B")        
+        #rospy.init_node('temp_state', anonymous=False)
         rospy.Subscriber('mobile_base/events/bumper',BumperEvent,self.BumperEventCallback)
 
-	print('Got here')
-	#TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
+        print('Got here')
+        #TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
 
         r = rospy.Rate(10);
 
         # Twist is a datatype for velocity
         move_cmd = Twist()
-	# let's go forward at 0.2 m/s
+        # let's go forward at 0.2 m/s
         move_cmd.linear.x = 0.2
-	# let's turn at 0 radians/s
-	move_cmd.angular.z = 0
+        # let's turn at 0 radians/s
+        move_cmd.angular.z = 0
 
-	# as long as you haven't ctrl + c keeping doing...
+        # as long as you haven't ctrl + c keeping doing...
         while not rospy.is_shutdown():
-	    print('a;lnkkl;n')
-	    if (self.bump_hit==1):
+            print('a;lnkkl;n')
+            if (self.bump_hit==1):
 
-		rospy.loginfo("sehejjy is life")
-		self.shutdown()
-	    # publish the velocity
+                rospy.loginfo("sehejjy is life")
+                self.shutdown()
+            # publish the velocity
             self.cmd_vel.publish(move_cmd)
-	    # wait for 0.1 seconds (10 HZ) and publish again
+            # wait for 0.1 seconds (10 HZ) and publish again
             r.sleep()
                         
         
     def shutdown(self):
         # stop turtlebot
         rospy.loginfo("Stop TurtleBot")
-	# a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
         self.cmd_vel.publish(Twist())
-	# sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
  
     def BumperEventCallback(self,data):
-	if(data.state == BumperEvent.PRESSED):
-		self.bump_hit = 1
+        if(data.state == BumperEvent.PRESSED):
+                self.bump_hit = 1
 
 if __name__ == '__main__':
-	GoForward()
+        GoForward()
