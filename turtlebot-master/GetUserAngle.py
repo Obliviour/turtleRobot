4435a3bbcc5c259ddead3f2172b59c95e862cbb5
sahluwalia6 <sahluwalia6@gatech.edu>
2018-10-22 18:16:20 -0400

    Added user input file

---
+++ turtlebot-master/GetUserAngle.py
@@ -0,0 +1,16 @@
+import rospy
+from std_msgs.msg import String
+
+def getAngle():
+    pub = rospy.Publisher('chatter', int, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    #rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+    	angle = input("Enter an Angle in Degrees from 0 to 360: ")
+    	if(angle > 360 || angle < 0):
+    		angle = angle % 360
+        pub.publish(angle)
+        #rate.sleep()
+
+if __name__ == '__main__':
+    getAngle()
