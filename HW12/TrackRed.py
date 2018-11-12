abf31c8c4c053fd4d56e5e4927a3c69d5535dc27
Obliviour <victor.barr@yahoo.com>
2018-11-12 16:25:58 -0500

    pep changes

--- HW12/TrackRed.py
+++ HW12/TrackRed.py
@@ -9,34 +9,36 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 
-class ShowCamera():
+class ShowCamera:
     def __init__(self):
-        # initiliaze
+        # initialize
         rospy.init_node('ShowCamera', anonymous=False)
         self.bridge = CvBridge()
         self.lower = np.array([10,1,100], dtype = "uint8")
         self.upper = np.array([38,15,255], dtype = "uint8")
         # rospy.init_node('CheckOdometry', anonymous=False)
         # we want a straight line with a phase of straight
-        rgb = rospy.Subscriber('/camera/rgb/image_raw', Image, self.displayRGB)
+        rgb = rospy.Subscriber('/camera/rgb/image_raw', Image, self.display_rgb)
         # tell user how to stop TurtleBot
         rospy.loginfo("To stop TurtleBot CTRL + C")
 
         rospy.spin()
 
-    def displayRGB(self,msg):
-        #rospy.loginfo("Received Image Data")
+    def display_rgb(self, msg):
+        """display rgb information"""
+        # rospy.loginfo("Received Image Data")
         try:
             image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
             rospy.loginfo("Converted color to cv2 image")
             mask = cv2.inRange(image, self.lower, self.upper)
-            #output = cv2.bitwise_and(image, image, mask = mask)
+            # output = cv2.bitwise_and(image, image, mask = mask)
             cv2.imshow("RBG Window", mask)
-	    #cv2.imwrite('TestImage.png',image)
+            # cv2.imwrite('TestImage.png',image)
             cv2.waitKey(1)
-	    #cv2.destroyWindow("RGB Window")
+            # cv2.destroyWindow("RGB Window")
         except CvBridgeError as e:
             print(e)
+
 
 if __name__ == '__main__':
     #try:
